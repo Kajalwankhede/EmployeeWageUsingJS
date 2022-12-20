5f1b01f6fbf7ef20deb86fdd4263ee3e3772dcd9
Kajalwankhede <kajuwankhede1510@gmail.com>
2022-12-20 19:59:00 +0530

    Ability to extnd Employee payroll dta to store gender and start date

--- EmployeePayRoll.js
+++ EmployeePayRoll.js
@@ -2,12 +2,16 @@ class EmployeePayRoll{
    
     id;          //property
     salary;
+    gender;
+    startDate;
 
  
-    constructor(id,name,salary){   //constructor
-        this.id=id;
-        this.name=name;
-        this.salary=salary;
+    constructor(...params){
+        this.id=params[0];
+        this.name=params[1];
+        this.salary=params[2];
+        this.gender=params[3];
+        this.startDate=params[4];
     }
 
     //getter and setter methods
@@ -22,7 +26,11 @@ class EmployeePayRoll{
     }
 
     toString(){                  
-        return "id="+this.id+" : name="+this.name+" : salary="+this.salary;
+        const options={year:'numeric', month:'long', day:'numeric'};
+        const empDate=this.startDate==undefined ? "undefined" :
+                        this.startDate.toLocaleDateString("en-US", options);
+        return "id = "+this.id + " : name = " + this.name+" : salary = " + this.salary+
+                " : gender = " + this.gender + " : Start Date = "+empDate;
     }
 }
 
@@ -30,3 +38,5 @@ let employeePayRoll = new EmployeePayRoll(1,"Mark",3000);
 console.log(employeePayRoll.toString());
 employeePayRoll.name="John";
 console.log(employeePayRoll.toString());
+let newEmployeePayRoll=new EmployeePayRoll(1,"Terrisa",30000,"Female",new Date());
+console.log(newEmployeePayRoll.toString());
